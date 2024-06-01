c489173414d279b2ade90e10098da94501939e80
ElenaNas <nas-elena@yandex.ru>
2024-06-02 00:28:02 +0200

    Implement MealServiceTest for JdbcMealRepository
    1)create MealTestData with static meals and implement additional methods (getNew, getUpdated etc);
    2)populate DB using populateDB.sql;
    3)create MealServiceTest, implement all test methods required.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -10,3 +10,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals(user_id,date_time, description, calories)
+VALUES (100000, '2024-06-01 08:00', 'Breakfast', 400),
+       (100000, '2024-06-01 12:30', 'Lunch', 600),
+       (100000, '2024-06-01 18:30', 'Dinner', 600);
+
+
---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,47 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+
+    public static final int MEAL_1_ID = START_SEQ + 3;
+    public static final int MEAL_2_ID = START_SEQ + 4;
+    public static final int MEAL_3_ID = START_SEQ + 5;
+
+    public static final int NOT_FOUND = 10;
+
+    public static final Meal meal1 = new Meal(MEAL_1_ID, LocalDateTime.of(2024, Month.JUNE, 1, 8, 0), "Breakfast", 400);
+    public static final Meal meal2 = new Meal(MEAL_2_ID, LocalDateTime.of(2024, Month.JUNE, 1, 12, 30), "Lunch", 600);
+    public static final Meal meal3 = new Meal(MEAL_3_ID, LocalDateTime.of(2024, Month.JUNE, 1, 18, 30), "Dinner", 600);
+
+    public static Meal getNew() {
+        return new Meal(null, LocalDateTime.of(2024, Month.JANUARY, 1, 10, 30), "Snack", 300);
+    }
+
+    public static Meal getUpdated() {
+        Meal updated = new Meal(MEAL_1_ID, meal1.getDateTime(), meal1.getDescription(), meal1.getCalories());
+        updated.setDateTime(LocalDateTime.of(2024, Month.JUNE, 1, 9, 10));
+        updated.setDescription("Second breakfast");
+        updated.setCalories(300);
+        return updated;
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+}
