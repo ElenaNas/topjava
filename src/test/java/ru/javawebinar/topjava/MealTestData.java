e5d3965d5e7411e6b16ee898e6d41e8ee013a02c
ElenaNas <nas-elena@yandex.ru>
2024-06-04 01:40:27 +0200

    Update test data for better readability.

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -20,12 +20,12 @@ public class MealTestData {
 
     public static final int NOT_FOUND = 10;
 
-    public static final Meal meal1 = new Meal(MEAL_1_ID, LocalDateTime.of(2024, Month.JUNE, 1, 8, 0), "Breakfast", 400);
-    public static final Meal meal2 = new Meal(MEAL_2_ID, LocalDateTime.of(2024, Month.JUNE, 1, 10, 0), "Second Breakfast", 150);
-    public static final Meal meal3 = new Meal(MEAL_3_ID, LocalDateTime.of(2024, Month.JUNE, 1, 12, 30), "Lunch", 600);
-    public static final Meal meal4 = new Meal(MEAL_4_ID, LocalDateTime.of(2024, Month.JUNE, 1, 16, 30), "Afternoon snack", 100);
-    public static final Meal meal5 = new Meal(MEAL_5_ID, LocalDateTime.of(2024, Month.JUNE, 1, 18, 30), "Dinner", 600);
-    public static final Meal meal6 = new Meal(MEAL_6_ID, LocalDateTime.of(2024, Month.JUNE, 1, 21, 30), "Late night snack", 100);
+    public static final Meal meal1 = new Meal(MEAL_1_ID, LocalDateTime.of(2024, Month.JUNE, 1, 8, 0), "USER - Breakfast", 400);
+    public static final Meal meal2 = new Meal(MEAL_2_ID, LocalDateTime.of(2024, Month.JUNE, 1, 10, 0), "USER - Second Breakfast", 150);
+    public static final Meal meal3 = new Meal(MEAL_3_ID, LocalDateTime.of(2024, Month.JUNE, 1, 12, 30), "USER - Lunch", 600);
+    public static final Meal meal4 = new Meal(MEAL_4_ID, LocalDateTime.of(2024, Month.JUNE, 1, 16, 30), "USER - Afternoon snack", 100);
+    public static final Meal meal5 = new Meal(MEAL_5_ID, LocalDateTime.of(2024, Month.JUNE, 1, 18, 30), "USER - Dinner", 600);
+    public static final Meal meal6 = new Meal(MEAL_6_ID, LocalDateTime.of(2024, Month.JUNE, 1, 21, 30), "USER - Late night snack", 100);
 
     public static Meal getNew() {
         return new Meal(null, LocalDateTime.of(2024, Month.JANUARY, 1, 10, 30), "Snack", 300);
@@ -34,7 +34,7 @@ public class MealTestData {
     public static Meal getUpdated() {
         Meal updated = new Meal(MEAL_1_ID, meal1.getDateTime(), meal1.getDescription(), meal1.getCalories());
         updated.setDateTime(LocalDateTime.of(2024, Month.JUNE, 1, 9, 10));
-        updated.setDescription("Second breakfast");
+        updated.setDescription("UPDATED: Second breakfast");
         updated.setCalories(300);
         return updated;
     }
