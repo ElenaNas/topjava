7e317e183f5eb1eb25c44ebdf219a696924f2a98
ElenaNas <nas-elena@yandex.ru>
2024-06-07 03:59:56 +0200

    Optimize code to use @Rule and @AfterClass efficiently
    1)remove classes StopwatchRule and SummaryRule;
    2)implement Stopwatch functionality to measure tests performance  using anonymous class and write it in the log;
    3)implement printResult with @AfterClass to write a summary of tests performance to the log.

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,8 +1,12 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -10,14 +14,16 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.rules.StopwatchRule;
-import ru.javawebinar.topjava.rules.SummaryRule;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertThrows;
+import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -31,14 +37,34 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 public class MealServiceTest {
 
+    private static final Logger log = getLogger("TestPerformance");
+
+    private static final Map<String, Long> testExecutionTimes = new HashMap<>();
+
     @Autowired
     private MealService service;
 
     @Rule
-    public StopwatchRule stopwatch = new StopwatchRule();
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String testName = description.getMethodName();
+            long elapsedTimeInMillis = TimeUnit.NANOSECONDS.toMillis(nanos);
+            log.info("{} - {} ms", testName, elapsedTimeInMillis);
+            testExecutionTimes.put(testName, elapsedTimeInMillis);
+        }
+    };
 
-    @Rule
-    public SummaryRule summaryRule = new SummaryRule();
+    @AfterClass
+    public static void printResult() {
+        if (!testExecutionTimes.isEmpty()) {
+            StringBuilder result = new StringBuilder("Test durations:\n");
+            for (Map.Entry<String, Long> entry : testExecutionTimes.entrySet()) {
+                result.append(entry.getKey()).append(" - ").append(entry.getValue()).append(" ms\n");
+            }
+            log.info(result.toString());
+        }
+    }
 
     @Test
     public void delete() {
