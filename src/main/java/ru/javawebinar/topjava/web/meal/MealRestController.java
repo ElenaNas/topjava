c7ef2241710b6cacca3ce39e7758c34c95661a59
ElenaNas <nas-elena@yandex.ru>
2024-06-21 18:43:19 +0200

    Move meal controllers to their respective package.

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,19 +1,12 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.web.AbstractMealController;
 
 @Controller
 public class MealRestController extends AbstractMealController {
-    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
-
-    private final MealService service;
 
     public MealRestController(MealService service) {
         super(service);
-        this.service = service;
     }
 }
