7615dcc7380635a0b1527a848898944a70e8de13
ElenaNas <nas-elena@yandex.ru>
2024-06-29 18:21:17 +0200

    Optimize code; rename package.

---
+++ src/main/java/ru/javawebinar/topjava/web/converter/StringToLocalDateConverter.java
@@ -0,0 +1,15 @@
+package ru.javawebinar.topjava.web.converter;
+import org.springframework.core.convert.converter.Converter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class StringToLocalDateConverter implements Converter<String, LocalDate> {
+
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+
+    @Override
+    public LocalDate convert(String dateString) {
+        return LocalDate.parse(dateString, DateTimeFormatter.ofPattern(DATE_PATTERN));
+    }
+}
