e9092ecbe966d2a1467f6199e2825ca8b335ef87
ElenaNas <nas-elena@yandex.ru>
2024-07-08 05:47:27 +0200

    Optimize UserService.toggleUserStatus.

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -60,6 +60,6 @@ public class UserService {
     public void toggleUserStatus(int id, boolean enabled) {
         User user = get(id);
         user.setEnabled(enabled);
-        update(user);
+        repository.save(user);
     }
 }
