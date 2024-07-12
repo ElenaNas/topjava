80ba3c2709219f336e3e1ba826e966132cb26ee0
ElenaNas <nas-elena@yandex.ru>
2024-07-12 02:45:45 +0200

    Refactor AdminUIController and MealUIController.

---
+++ src/main/java/ru/javawebinar/topjava/util/ResponseEntityErrorHandling.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.util;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import java.util.stream.Collectors;
+
+public class ResponseEntityErrorHandling {
+    public static ResponseEntity<String> handleBindingResultErrors(BindingResult result) {
+        if (result.hasErrors()) {
+            String errorFieldsMsg = result.getFieldErrors().stream()
+                    .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                    .collect(Collectors.joining("<br>"));
+            return ResponseEntity.unprocessableEntity().body(errorFieldsMsg);
+        }
+        return null;
+    }
+}
