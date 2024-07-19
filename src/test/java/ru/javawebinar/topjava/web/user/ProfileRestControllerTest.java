508e335ff2f9265fcaeb553c0516dba01cdd22b8
ElenaNas <nas-elena@yandex.ru>
2024-07-19 16:23:31 +0200

    Implement controllers validation.
    1)add @Valid in REST controllers;
    2)refactor createOrUpdate in UI controllers;
    3)modify ErrorInfo.detail type;
    4)add ExceptionInfoHandler.bindValidationError;
    5)add varargs String detail to ExceptionInfoHandler.logAndGetErrorInfo;
    6)add tests for invalid data.

--- src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
@@ -86,4 +86,25 @@ class ProfileRestControllerTest extends AbstractControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(USER_WITH_MEALS_MATCHER.contentJson(user));
     }
+
+    @Test
+    void registerWithInvalidData() throws Exception {
+        UserTo newTo = new UserTo(null, null, null, null, 0);
+        perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    void updateWithInvalidData() throws Exception {
+        UserTo updatedTo = new UserTo(null, null, "123456", null, 0);
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(user))
+                .content(JsonUtil.writeValue(updatedTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
 }
