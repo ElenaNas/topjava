8ab5d0fdec1fd878a6d229c52a9cbbbefe6de56d
ElenaNas <nas-elena@yandex.ru>
2024-07-23 13:14:19 +0200

    Refactor EmailValidator.validate.

--- src/main/java/ru/javawebinar/topjava/web/user/EmailValidator.java
+++ src/main/java/ru/javawebinar/topjava/web/user/EmailValidator.java
@@ -4,7 +4,7 @@ package ru.javawebinar.topjava.web.user;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-import ru.javawebinar.topjava.ValidEmail;
+import ru.javawebinar.topjava.HasEmail;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.web.ExceptionInfoHandler;
@@ -21,17 +21,22 @@ public class EmailValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return ValidEmail.class.isAssignableFrom(clazz);
+        return HasEmail.class.isAssignableFrom(clazz);
     }
 
     @Override
     public void validate(Object target, Errors errors) {
-        ValidEmail user = ((ValidEmail) target);
+        HasEmail user = ((HasEmail) target);
         User newUser = repository.getByEmail(user.getEmail());
         if (newUser != null) {
-            if (!newUser.getEmail().equals(user.getEmail())) {
+            if (user.getId() != null) {
+                int id = newUser.id();
+                if (user.getId() != null && id == user.id()){
+                    return;
+                }
                 errors.rejectValue("email", ExceptionInfoHandler.EXCEPTION_DUPLICATE_EMAIL);
             }
         }
     }
 }
+
